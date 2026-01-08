cbb0c8f018933bd7284be6f19db05a4fda4a58c9
Raj Sonawane <rajsonawane7499@gmail.com>
2026-01-08 18:10:07 +0000

    building featured card section

--- musicnextjs/src/app/layout.tsx
+++ musicnextjs/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar"
+import { Navbar } from "@/components/Navbar"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
+        <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+        </div>
         {children}
       </body>
     </html>
