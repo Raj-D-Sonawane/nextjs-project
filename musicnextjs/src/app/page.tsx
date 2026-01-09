282478aaaef8516165aca29248ac3d69401282aa
Raj Sonawane <rajsonawane7499@gmail.com>
2026-01-09 04:42:51 +0000

    Designing Sticky scroll

--- musicnextjs/src/app/page.tsx
+++ musicnextjs/src/app/page.tsx
@@ -1,4 +1,6 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import { main } from "framer-motion/client";
 
 
@@ -8,6 +10,8 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
 
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
